1841e496de1e15de907dd58d4f21e46e41cbc392
Samsbot <164393128+Samsbot@users.noreply.github.com>
2024-08-01 21:19:10 +0300

    SAMS

--- commandes/Ibrahim.js
+++ commandes/Ibrahim.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { france } = require("../framework/france");
-france({ nomCom: "keith", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+france({ nomCom: "Sams", reaction: "🦂", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Yes you called me whats up Ok anyway support me by following my channel and subscribing on my youtube channel 🙏 \n\n ' + "║❒ 𝙒𝘼𝘾𝙃𝘼𝙉𝙉𝙀𝙇 https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47";
-    let d = '                                     ║❒ 𝗬𝗼𝘂𝗧𝘂𝗯𝗲 https://www.youtube.com/@keithkeizzah';
+    let z = 'Yes you called me what's up mah friend🧐 Anyway support me by following my channel and subscribing on my youtube channel 🙏 \n\n ' + "║❒ 𝙒𝘼𝘾𝙃𝘼𝙉𝙉𝙀𝙇 https://whatsapp.com/channel/0029VafWZf5GufInRAgb063L";
+    let d = '                                     ║❒ 𝗬𝗼𝘂𝗧𝘂𝗯𝗲 https://youtube.com/@samslaurent?si=v4KeNBEi1TTxoP1c';
     let varmess = z + d;
     var mp4 = 'https://telegra.ph/file/08f740224ed39233f92cb.mp4';
     await zk.sendMessage(dest, { video: { url: mp4 }, caption: varmess });
@@ -19,8 +19,8 @@ console.log("mon test");
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *BMW-MD-REPO* \n\n '+'je suis un bot Whatsapp Multi-appareil '
-      let d =' developpé par *BMW-MD-REPO*'
+   let z ='Salut je m\'appelle *HUNCHO-MD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *HUNCHO-MD*'
       let varmess=z+d
       var img='https://telegra.ph/file/13d63c21c1a665bfd8324.jpg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
